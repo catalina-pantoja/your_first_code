2d0a782bd00501f39091d2994a18b84ff94132ec
catalina-pantoja <81584544+catalina-pantoja@users.noreply.github.com>
2021-03-29 13:08:32 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
-}
+int main(Hello School)
+{Hello School
+  int i;Hello School
+  for (i = 0; i < 10; i++)Hello School
+  {Hello School
+    printf("School\n");Hello School
+  }Hello School
+  return 0;Hello School
+}Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
+Hello School
